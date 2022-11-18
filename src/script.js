bc513a40b7a4ac9ae216ebb08f1aa1aa35068810
nico <westernfarthing@gmail.com>
2022-11-17 22:49:36 -0600

    added day display

---
+++ src/script.js
@@ -0,0 +1,37 @@
+// ids
+// display the date
+var todaysDate = moment("11/3/20", "MM-DD-YY").format("dddd, MMMM Do YYYY, h:mm:ss a");
+$("#currentDay").text(todaysDate);
+
+
+// // Initial Commit
+// var formEl = $('#skills-form');
+// var nameInputEl = $('#skill-name');
+// var dateInputEl = $('#datepicker');
+// var skillsListEl = $('#skills-list');
+
+// var printSkills = function (name, date) {
+//   var listEl = $('<li>');
+//   var listDetail = name.concat(' on ', date);
+//   listEl.addClass('list-group-item').text(listDetail);
+//   listEl.appendTo(skillsListEl);
+// };
+
+// var handleFormSubmit = function (event) {
+//   event.preventDefault();
+
+//   var nameInput = nameInputEl.val();
+//   var dateInput = dateInputEl.val();
+
+//   if (!nameInput || !dateInput) {
+//     console.log('You need to fill out the form!');
+//     return;
+//   }
+
+//   printSkills(nameInput, dateInput);
+
+//   nameInputEl.val('');
+//   dateInputEl.val('');
+// };
+
+// formEl.on('submit', handleFormSubmit);
